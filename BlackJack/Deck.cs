0a324fa8e40ff4f5cc9e8824e49b8813baed130f
Jonathan <irwinjonathan3@gmail.com>
2022-06-20 11:41:21 +0100

    each card can now be created when the deck is instantiated

--- BlackJack/Deck.cs
+++ BlackJack/Deck.cs
@@ -14,23 +14,23 @@ public class Deck
         string[] suits = { "Hearts", "Clubs", "Spades", "Diamonds" };
         for (int i = 0; i < 4; i++)
         {
-            D[0 + i * 13] = new Card("Ace of " + suits[i], 1, 11);
-            D[1+ i * 13] = new Card("Two of "+ suits[i],2);
-            D[2+ i * 13] = new Card("Three of " + suits[i], 3);
-            D[3+ i * 13] = new Card("Four of " + suits[i], 4);
-            D[4+ i * 13] = new Card("Five of " + suits[i], 5);
-            D[5+ i * 13] = new Card("Six of " + suits[i], 6);
-            D[6+ i * 13] = new Card("Seven of " + suits[i], 7);
-            D[7+ i * 13] = new Card("Eight of " + suits[i], 8);
-            D[8+ i * 13] = new Card("Nine of " + suits[i], 9);
-            D[9+ i * 13] = new Card("Ten of " + suits[i], 10);
-            D[10+ i * 13] = new Card("Jack of " + suits[i], 10);
-            D[11+ i * 13] = new Card("Queen of " + suits[i], 10);
-            D[12+ i * 13] = new Card("King of " + suits[i], 10);
+            d[0 + i * 13] = new Card("Ace of " + suits[i], 1, 11);
+            d[1+ i * 13] = new Card("Two of "+ suits[i],2);
+            d[2+ i * 13] = new Card("Three of " + suits[i], 3);
+            d[3+ i * 13] = new Card("Four of " + suits[i], 4);
+            d[4+ i * 13] = new Card("Five of " + suits[i], 5);
+            d[5+ i * 13] = new Card("Six of " + suits[i], 6);
+            d[6+ i * 13] = new Card("Seven of " + suits[i], 7);
+            d[7+ i * 13] = new Card("Eight of " + suits[i], 8);
+            d[8+ i * 13] = new Card("Nine of " + suits[i], 9);
+            d[9+ i * 13] = new Card("Ten of " + suits[i], 10);
+            d[10+ i * 13] = new Card("Jack of " + suits[i], 10);
+            d[11+ i * 13] = new Card("Queen of " + suits[i], 10);
+            d[12+ i * 13] = new Card("King of " + suits[i], 10);
         }
     }
     
-    public Card[] D
+    public Card[] deck
     {
         get => d;
         set => d = value ?? throw new ArgumentNullException(nameof(value));
--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -1,4 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BlackJack;
+
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
+
+Deck deck = new Deck();
+
+for (int i = 0; i < deck.deck.Length; i++)
+{
+    if (deck.deck[i].Value != 1)
+    {
+        Console.WriteLine(deck.deck[i].Name + " " + deck.deck[i].Value);
+    }
+    else
+    {
+        Console.WriteLine(deck.deck[i].Name + " " + deck.deck[i].Value + " " + deck.deck[i].SecondaryValue);
+    }
+}
+
+
 
